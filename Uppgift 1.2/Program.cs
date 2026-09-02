f318111bdddfaa5267a69d5050666ac816ac4f3f
Gustav-Wahren <gustavwahren8@gmail.com>
2026-09-02 15:13:51 +0200

    Add project files.

---
+++ Uppgift 1.2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Uppgift_1._2;
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.BackgroundColor = ConsoleColor.Blue;
+        Console.Clear();
+        Console.WriteLine(":(");
+        Console.WriteLine("A problem has been detected and Windows has been shut down to prevent damage to your computer.");
+    }
+}
